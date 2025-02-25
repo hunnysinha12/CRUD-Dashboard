2c193403063dbef239e4ebd01fa0e8d63dc03f72
JYOTY BHUSHAN SINHA <hunnysinha127@gmail.com>
2025-02-25 20:39:28 +0530

    placeholder configure

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -47,7 +47,7 @@ export default function SignUp() {
             <div className="mb-4">
               <input
                 type="text"
-                placeholder="Hunny"
+                placeholder="Enter your name"
                 value={fullName}
                 onChange={(e) => setFullName(e.target.value)}
                 className="text-black w-full p-3 border border-gray-300 rounded-md"
